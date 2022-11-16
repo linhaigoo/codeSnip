e192542caabf786629b6ce3dcbf5f29ebba11c95
linhaigoo <linhaigoo2013@gmail.com>
2022-11-17 00:11:36 +0800

    [add] distract pointer of  member function and then invoke it

--- src/virtual_item7/pure_virtual.cpp
+++ src/virtual_item7/pure_virtual.cpp
@@ -2,7 +2,7 @@
  * @Author: linhaigoo linhaigoo2013@gmail.com
  * @Date: 2022-09-18 21:07:46
  * @LastEditors: linhaigoo linhaigoo2013@gmail.com
- * @LastEditTime: 2022-10-15 17:14:39
+ * @LastEditTime: 2022-11-17 00:09:10
  * @FilePath: /codeSnip/src/virtual_item7/pure_virtual.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -46,6 +46,8 @@ int main() {
   //   const void *delfuncPtr = &v1.~AWOV;
   //   void (*delfuncPtr)(AWOV *) = &AWOV::func;
   void (AWOV::*delfuncPtr)() = &AWOV::func;
+  printf("call member function by its member function pointer!!!\n");
+  (v1.*delfuncPtr)(); // call member function by its member function pointer!!!
   v1.~AWOV();
 
   printf("delfuncPtr %p \n", delfuncPtr);
